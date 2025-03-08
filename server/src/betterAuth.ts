4f43978b561f0b1d08a6aa4911ccaabd47bc7e3c
white <grigsgt@gmail.com>
2025-03-08 16:44:25 +0300

    создал плагин для авторизации и проверки авторизован ли user

---
+++ server/src/betterAuth.ts
@@ -0,0 +1,25 @@
+import {Elysia} from "elysia";
+import {auth} from "../lib/auth";
+
+const betterAuth = new Elysia({name: 'better-auth'})
+    .mount(auth.handler)
+    .macro({
+        auth: {
+            async resolve({error, request: {headers}}) {
+                const session = await auth.api.getSession({
+                    headers
+                })
+
+                if (!session)
+                    return error(401)
+
+                return {
+                    user: session.user,
+                    session: session.session
+                }
+            }
+        }
+    })
+    .as('plugin')
+
+export default betterAuth
