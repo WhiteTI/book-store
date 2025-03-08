3498f8dcdfc6b72675df733532c28f59d9d3e175
white <grigsgt@gmail.com>
2025-03-08 16:45:09 +0300

    включил плагин и маршруты в основной экземляр приложения

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,31 @@
 import { Elysia } from "elysia";
+import cors from "@elysiajs/cors";
+import betterAuth from "./betterAuth";
+import bookTypes from "./routes/bookTypes";
+import genres from "./routes/genres";
+import series from "./routes/series";
+import books from "./routes/books";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+// const betterAuthView = ({request, error}: Context) => {
+//     const BETTER_AUTH_ACCEPT_METHODS = ['GET', 'POST', 'PATCH', 'DELETE']
+//
+//     if (BETTER_AUTH_ACCEPT_METHODS.includes(request.method)) {
+//         return auth.handler(request)
+//     } else {
+//         error(405)
+//     }
+// }
+
+const app = new Elysia()
+    // .all('/api/auth/*', (ctx) => betterAuthView(ctx))
+    .use(cors())
+    .use(betterAuth)
+    .use(bookTypes)
+    .use(genres)
+    .use(series)
+    .use(books)
+    .get("/", () => "Hello Elysia")
+    .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
