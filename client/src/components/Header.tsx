cbfd5aebf25bce37465e9c88b900c5606d4ec251
white <grigsgt@gmail.com>
2025-03-17 22:43:18 +0300

    создал компонент header

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import classes from '../styles/Header.module.css'
+
+const Header = () => {
+    return (
+        <header className={classes.bg}>
+            <div className='container'>
+                <nav className={classes.navMenu}>
+
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
